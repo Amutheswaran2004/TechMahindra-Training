0fc35a671df328f09724f8f21abd184de36aafd2
Amutheswaran <amudheswaran@gmail.com>
2025-02-08 08:10:47 +0530

    Add files via upload

---
+++ Week 2 Task/Final keyword/FinalClass.java
@@ -0,0 +1,8 @@
+final class FinalClass {
+    public final int constantValue = 777;
+
+    public final void display() {
+        System.out.println("This is belongs to final method in Final class.");
+    }
+}
+
