59f8694ba3c10c32b803ceb50cfb3bbedaf3017d
Amutheswaran <amudheswaran@gmail.com>
2025-02-10 14:23:55 +0530

    Add files via upload

---
+++ Monday-10-02-2025/Task_2/JavaIOAssignment1.java
@@ -0,0 +1,10 @@
+package Task_2;
+
+public class JavaIOAssignment1{
+    public static void main(String[] args) {
+        Print.print();
+        Print.println();
+        Print obj=new Print();
+        obj.error();
+    }
+}
---
+++ Monday-10-02-2025/Task_2/Print.java
@@ -0,0 +1,18 @@
+package Task_2;
+
+public class Print {
+    public static void print(){
+        System.out.print("My Name ");
+        System.out.print(" is ");
+    }
+    public static void println(){
+        System.out.println(" Amudheswaran");
+        System.out.println("ECE-E");
+        System.out.println("PEC");
+    }
+    public  void error(){
+        System.err.println("Warning");
+        System.err.println("Invalid Statement");
+    }
+
+}
